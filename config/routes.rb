1efbfbdf6b36fdf7cb4bc8aedf568f9d5e341a30
Ryoma-23 <120545081+Ryoma-23@users.noreply.github.com>
2023-01-16 12:08:38 +0000

    [Add] 投稿機能、詳細画面、編集画面、一覧画面

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
 
-  resources :books, only: [:index, :create, :show, :edit]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
   get 'home/about' => 'homes#about', as: 'about'
+
 end
