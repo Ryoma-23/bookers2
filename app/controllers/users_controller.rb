78d828bbdabf48b2a1bd431a04faaf56429c5b6a
Ryoma-23 <120545081+Ryoma-23@users.noreply.github.com>
2023-01-18 11:33:30 +0000

    [Add] nameでログイン、ページ完成、残りレイアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
     @books = @user.books
@@ -34,5 +35,12 @@ class UsersController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to post_images_path
+    end
   end
 end
