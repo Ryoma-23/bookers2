1a15082dd9676cf92e477b06517edb97443ab892
Ryoma-23 <120545081+Ryoma-23@users.noreply.github.com>
2023-01-18 08:21:04 +0000

    [Add] バリデーション、権限、フラッシュメッセージの設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,11 +8,18 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    @user = @book.user
+    @books = Book.all
+    if @book.save
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def show
+    @bookk = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
   end
@@ -22,9 +29,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
+
   end
 
   def index
     @user = current_user
     @users = User.all
+    @book = Book.new
   end
 
   def edit
@@ -15,13 +18,21 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      flash[:notice] = "successfully"
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
